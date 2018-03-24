2868e9bc6ed1da0ac57c201ee6a50a31dd72cac7
Raymond Cohen <raycohen@gmail.com>
2018-03-24 13:03:13 -0400

    add source-map-loader

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -176,6 +176,13 @@ module.exports = {
               cacheDirectory: true,
             },
           },
+          {
+            test: /\.js$/,
+            use: [
+              require.resolve('source-map-loader')
+            ],
+            enforce: 'pre'
+          },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
           // "style" loader turns CSS into JS modules that inject <style> tags.
