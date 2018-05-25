06c029a6a18cd06c93270ee87e9f38cf2cad0647
Ray Cohen <rcohen@paperlesspost.com>
2018-05-25 13:13:42 -0400

    add babel-plugin-styled-components via webpack config files

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -171,6 +171,7 @@ module.exports = {
               babelrc: false,
               presets: [require.resolve('babel-preset-react-app')],
               // @remove-on-eject-end
+              plugins: [require.resolve('babel-plugin-styled-components')],
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
